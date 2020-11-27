7b62ecff2548863ab871533bb856951758a90a05
Kateryna Shyshkina <ekateryna.shyshkina@gmail.com>
2020-11-27 13:46:33 +0200

    added method for checking

--- src/main/java/task/Solution.java
+++ src/main/java/task/Solution.java
@@ -1,5 +1,6 @@
 package task;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,6 +12,12 @@ public class Solution {
                 .distinct()
                 .filter(secondList::contains)
                 .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        List<Integer> firstList = Arrays.asList(1, 2, 2, 5, 7, 5, 8, 1, 3);
+        List<Number> secondList = Arrays.asList(9, 3, 2, 4, 10, 1, 5, 1, 2, 2, 4);
+        System.out.println(findDuplicates(firstList, secondList));
     }
 
 }
